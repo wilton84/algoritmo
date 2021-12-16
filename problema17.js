a8121549e1834eefb5e7762dfc3bb1ab1710cc2b
wilton84 <wiltondelgado85@gmail.com>
2021-12-16 12:01:22 -0400

    problema17

---
+++ problema17.js
@@ -0,0 +1,7 @@
+var ecuCuad = function(a, b, c) {
+    var x1 = (-b + Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
+    var x2 = (-b - Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
+    return x1 + " " + x2;
+}
+
+ecuCuad(1, 2, 1);
